7cea565cf38b99373dcafa55f5a238bcbb6c6b67
Nithin Pingili <nithinp300@gmail.com>
2019-08-12 12:57:46 -0500

    filtered links to only include article headlines

--- articles.py
+++ articles.py
@@ -1,19 +1,33 @@
 from bs4 import BeautifulSoup
 import requests
 
-url = "https://thenextweb.com/"
+url = "https://techcrunch.com/"
 response = requests.get(url, timeout=5)
 content = BeautifulSoup(response.content, "html.parser")
 
-h2_tags = content.findAll('h')
+h2_tags = content.findAll('h2')
+h3_tags = content.findAll('h3')
 h2_tags_text = []
 for h2 in h2_tags:
 	h2_tags_text.append(h2.get_text().strip())
 
+# method to get A tags from header tags (h1, h2, h3...)
+def get_a_tags(a_tags, h_tags):
+	for h in h_tags:
+	    if not h.a is None:
+	        a_tags.append(h.a)
+
 a_tags = []
-for h2 in h2_tags:
-    if not h2.a is None:
-        a_tags.append(h2.a)
+get_a_tags(a_tags, h2_tags)
+get_a_tags(a_tags, h3_tags)
+
+articles = []
 for a in a_tags:
-	print(a.get_text())
+	headline = a.get_text().strip()
+	link = a.get('href')
+	if url in link:
+		articles.append(a)
+	#print(a.get('href'))
+for a in articles:
+	print(a.get_text().strip())
 	#print(a.get('href'))
